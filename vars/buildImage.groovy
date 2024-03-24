f1fd2d6610251de871ebcd12a55cee39e53579d2
ILO2003 <ilkjavaxishvili@gmail.com>
2024-03-24 01:22:56 +0100

    Update buildImage.groovy with my credentials

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
 def call() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+    echo "building the docker image ..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
+    sh 'docker build -t ilo2003/testing:jma-2.0 .'
+    sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+    sh 'docker push ilo2003/testing:jma-2.0'
     }
 }
